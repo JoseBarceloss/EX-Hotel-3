2422ee171ba4d6b20c825313f20badaa78c66853
Barcelos <barcelos123@Ubuntu.myguest.virtualbox.org>
2024-02-25 22:54:47 -0300

    requesito um completo

---
+++ src/01_challenge.sql
@@ -0,0 +1,24 @@
+--Vou Comentar porque no Projeto eu olho o repositorio para me lembrar de comandos etc
+--e tem uns comando que talvez no dia do projeto eu nao lembre.
+--
+-- Criação da tabela hotels
+CREATE TABLE hotels (
+    id INT AUTO_INCREMENT PRIMARY KEY, -- Coluna para o ID, auto incremento e chave primária
+    name VARCHAR(80) NOT NULL, -- Nome do hotel, não pode ser nulo
+    zip_code VARCHAR(8) NOT NULL, -- CEP do hotel, não pode ser nulo
+    city VARCHAR(80), -- Cidade do hotel
+    state VARCHAR(80), -- Estado do hotel
+    district VARCHAR(80), -- Distrito do hotel
+    street VARCHAR(80), -- Rua do hotel
+    star INT NOT NULL -- Classificação do hotel, não pode ser nulo
+);
+
+-- Inserção dos dados na tabela hotels
+INSERT INTO hotels (name, zip_code, city, state, district, street, star) VALUES
+('Brown, Kihn and Bergnaum', '61749464', 'Christiansenhaven', 'Ohio', 'South', 'Myrna Ports 188', 1), -- Hotel 1
+('Dickinson, Mayer and Boyer', '79345910', 'Abbottshire', 'NorthDakota', 'Lake', 'Tommie Street 781', 3), -- Hotel 2
+('Hettinger-Raynor', '29551210', 'Pollichborough', 'Wyoming', 'Port', 'Emerson Square 494', 3), -- Hotel 3
+('Ernser Inc', '16541278', 'Kodymouth', 'Nebraska', 'East', 'Bart Knolls 161', 4); -- Hotel 4
+
+-- Seleção de todos os dados da tabela hotels
+SELECT * FROM hotels;
